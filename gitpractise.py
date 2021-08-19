58ebcc5724eafb68ca08642acaae9a3776b82884
njirarukia <njirarukia@gmail.com>
2021-08-19 20:12:43 +0300

    Git Practise

---
+++ gitpractise.py
@@ -0,0 +1,10 @@
+#Assigment 1
+#By Rukia
+def main():
+        print("Hello Git!")
+        print("Hello Again")
+        print('Yay!Printing.')
+        print("I'd much rather you'not'.")
+        print('"said"do not touch this.')
+
+main()
